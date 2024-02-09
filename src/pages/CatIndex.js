2621327450a21c6ee13863dee6200b7e4213e31a
jwat5623 <jamar34watson56@gmail.com>
2024-02-09 14:19:08 -0800

    adds creation functionality

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -4,24 +4,24 @@ import { NavLink as RouterNavLink } from "react-router-dom"
 const CatIndex = ({ cats }) => {
   return (
     <>
-    <h3>Meet All the Cats</h3>
-    <div className="cat-index-section">
-      {cats?.map((cat) => {
-        return (
-          <div key={cat.id} className="cat-profile-container">
-            <img 
-              src={cat.image}
-              alt="profile of all our cats friends"
-              className="cat-profile-pic"
-            />
-            <RouterNavLink to={`/catshow/${cat.id}`}>
-              <p>{cat.name}</p>
-            </RouterNavLink>
-          </div>
-        )
-      })}
-    </div>
-  </>
-)
+      <h3>Meet All the Cats</h3>
+      <div className="cat-index-section">
+        {cats?.map((cat) => {
+          return (
+            <div key={cat.id} className="cat-profile-container">
+              <img 
+                src={cat.image}
+                alt="profile of all our cats friends"
+                className="cat-profile-pic"
+              />
+              <RouterNavLink to={`/catshow/${cat.id}`}>
+                <p>{cat.name}</p>
+              </RouterNavLink>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 export default CatIndex
--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Form, FormGroup, Input, Button } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
-const CatNew = () => {
+
+const CatNew = ({ createCat }) => {
+  const navigate = useNavigate()
+  const [newCat, setNewCat] = useState ({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: ""
+  })
+
+
+  const handleChange = (e) => {
+    setNewCat({ ...newCat, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = () => {
+    createCat(newCat)
+    navigate("/catindex")
+  }
   return (
-      <div>CatNew</div>
+    <div>
+      <h3>Add Your Info To Make New Friends</h3>
+      <Form>
+        <FormGroup>
+          <Input
+            id="name"
+            name="name"
+            placeholder="Enter Your Cat's Name"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            id="age"
+            name="age"
+            placeholder="Enter Your Cat's Age"
+            type="number"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            id="enjoys"
+            name="enjoys"
+            placeholder="Enter What Your Cat Likes To Do"
+            type="textarea"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            id="image"
+            name="image"
+            placeholder="Enter the URL of Your Cat's Image"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <Button onClick={handleSubmit}>Submit Cat</Button>
+      </Form>
+    </div>
   )
 }
 
