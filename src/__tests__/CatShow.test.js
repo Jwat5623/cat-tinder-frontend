de3fa95731530330c5389cc75aed68e05d01146f
jwat5623 <jamar34watson56@gmail.com>
2024-02-09 16:28:18 -0800

    adds test for creation

--- src/__tests__/CatShow.test.js
+++ src/__tests__/CatShow.test.js
@@ -13,12 +13,13 @@ describe("<CatShow />", () => {
         </Routes>
       </MemoryRouter>
     )
-
-    const catName = screen.getByText(`Meet ${mockCats[0].name}`)
+    const catName = screen.getByText(/name:oscar/i)
     expect(catName).toBeInTheDocument()
-    const catAge = screen.getByText(mockCats[0].age)
+    const catAge = screen.getByText(/age: 5/i)
     expect(catAge).toBeInTheDocument()
-    const catEnjoys = screen.getByText(mockCats[0].enjoys)
+    const catEnjoys = screen.getByText(
+      /swipe right if youre feline fine and ready to mingle! im a whisker\-twitching aficionado of belly rubs and cardboard boxes, seeking a purr\-fect partner for lazy afternoons and midnight zoomies\./i
+      )
     expect(catEnjoys).toBeInTheDocument()
   })
 })
